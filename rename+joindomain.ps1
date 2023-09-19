4afcf3706fcd3624bcebf9869d55aabc92499448
Pawel Kostelnik <pkostelnik@gmail.com>
2023-09-19 10:23:15 +0200

    another commit

--- rename+joindomain.ps1
+++ rename+joindomain.ps1
@@ -1,6 +1,6 @@
 ﻿$Domain = "ent16.lab"
 $User = "Administrator"
-$NewName = "EX3"
+$NewName = "sp"
 $DC = "DC"
 
 Add-Computer -DomainName $Domain -Server "$DC.$Domain" -ComputerName "$env:COMPUTERNAME" -Credential "$Domain\$User" -newname $NewName -Restart
